f9bd94af0c366c08efbbb9b993a1864ac0c5b855
Samuelson Owei <97484454+Sammyowei@users.noreply.github.com>
2023-11-16 14:31:56 +0100

    wow finished player presistence

--- lib/src/player_progress/player_progress.dart
+++ lib/src/player_progress/player_progress.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:js_util';
 
 import 'package:flutter/foundation.dart';
 
@@ -6,11 +7,12 @@ import 'presistence/player_progress_persistence.dart';
 
 /// Encapsulates the player's progress.
 class PlayerProgress extends ChangeNotifier {
-  static const maxHighestScoresPerPlayer = 10;
+  // static const maxHighestScoresPerPlayer = 10;
 
   final PlayerProgressPersistence _store;
 
   int _highestLevelReached = 0;
+  int _highestCoinsCollected = 0;
 
   /// Creates an instance of [PlayerProgress] backed by an injected
   /// persistence [store].
@@ -19,14 +21,23 @@ class PlayerProgress extends ChangeNotifier {
   /// The highest level that the player has reached so far.
   int get highestLevelReached => _highestLevelReached;
 
+  int get coinsAmount => _highestCoinsCollected;
+
   /// Fetches the latest data from the backing persistence store.
   Future<void> getLatestFromStore() async {
     final level = await _store.getHighestLevelReached();
+    final coins = await _store.getCoinValue();
     if (level > _highestLevelReached) {
       _highestLevelReached = level;
       notifyListeners();
     } else if (level < _highestLevelReached) {
       await _store.saveHighestLevelReached(_highestLevelReached);
+    }
+    if (coins > _highestCoinsCollected) {
+      _highestCoinsCollected = coins;
+      notifyListeners();
+    } else if (coins < _highestCoinsCollected) {
+      await _store.saveCoins(_highestCoinsCollected);
     }
   }
 
@@ -34,8 +45,10 @@ class PlayerProgress extends ChangeNotifier {
   /// playing the game for the first time.
   void reset() {
     _highestLevelReached = 0;
+    _highestCoinsCollected = 0;
     notifyListeners();
     _store.saveHighestLevelReached(_highestLevelReached);
+    _store.saveCoins(_highestCoinsCollected);
   }
 
   /// Registers [level] as reached.
@@ -48,6 +61,17 @@ class PlayerProgress extends ChangeNotifier {
       notifyListeners();
 
       unawaited(_store.saveHighestLevelReached(level));
+    }
+  }
+
+  void setCoinsCollected(int coins) {
+    if (coins > _highestCoinsCollected) {
+      _highestCoinsCollected = coins;
+      notifyListeners();
+
+      unawaited(
+        _store.saveCoins(coins),
+      );
     }
   }
 }
