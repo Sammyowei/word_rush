b769095449c22ae618640710da14eec0115c8d1f
Samuelson Owei <97484454+Sammyowei@users.noreply.github.com>
2023-11-17 15:02:00 +0100

    added the levels

--- lib/src/screens/level_selection.dart
+++ lib/src/screens/level_selection.dart
@@ -27,6 +27,7 @@ class LevelSelection extends StatelessWidget {
             ),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               CustomAppBar(
                 color: palette,
@@ -40,7 +41,79 @@ class LevelSelection extends StatelessWidget {
                   context.pop();
                 },
                 rightBtnPath: image.prevButton,
-              )
+              ),
+
+              // TODO: LevelSelection body
+
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(image.levelHeader),
+                    Container(
+                      height: 400,
+                      width: MediaQuery.sizeOf(context).width,
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(
+                            image.settingBoard,
+                          ),
+                        ),
+                      ),
+                      child: GridView.builder(
+                        itemCount: gameLevels.length,
+                        clipBehavior: Clip.antiAlias,
+                        padding: const EdgeInsets.all(20),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 30,
+                          mainAxisSpacing: 30,
+                        ),
+                        itemBuilder: (context, index) {
+                          final level = gameLevels[index];
+                          final levelEnabled = (playerProgress
+                                      .highestLevelReached >=
+                                  level.level - 1 &&
+                              playerProgress.coinsAmount >= level.coinValue);
+                          return Container(
+                            clipBehavior: Clip.antiAlias,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              shadows: [
+                                BoxShadow(
+                                  color: palette.backgroundSettings,
+                                  blurRadius: 7,
+                                  spreadRadius: 1,
+                                  offset: const Offset(1, 1),
+                                )
+                              ],
+                              color: palette.btnColor,
+                              image: DecorationImage(
+                                scale: 1,
+                                matchTextDirection: true,
+                                alignment: Alignment.center,
+                                image: AssetImage(
+                                  image.levelTable1,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // TODO: Footer Do not modify
+              Container(),
+              Container(),
+              Container(),
             ],
           ),
         ),
